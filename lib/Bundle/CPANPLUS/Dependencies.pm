c32fa87bd920eaaa0971dc436b3c3e367198e27f
Jos Boumans <kane@cpan.org>
2006-08-17 20:48:45 +0000

    r2121@codebase2:  kane | 2006-08-17 22:07:39 +0200  Commit history from perforce during svk import:
     Change 12596 on 2006/08/02 by kane@myriad
      
      	   * this be 0.073_01, dev release
      	   * update manifest
      	   * update dependencies
      	   * update changes
      
      Change 12594 on 2006/08/02 by kane@myriad
      
      	   * up dependency versions
      
      Change 12584 on 2006/08/01 by kane@myriad
      
      	   * up dependency versions for required features
      
      Change 12575 on 2006/08/01 by kane@myriad
      
      	   * update the dependencies to include m::l::c 0.10
      	   * move the cpanplus ones to the end. doesn't actually change order in CPANPLUS though
      	   * up version to 0.05
      
      Change 12502 on 2006/06/06 by kane@myriad
      
      	   * up dependencies to at least 0.01, so we compare versions against our bundled ones
      	   * remove obsolete use of cpanplus::inc in default shell
      
      Change 12404 on 2006/03/09 by kane@coke
      
      	   * add the 'new' depdendencies
      
      Change 12127 on 2005/07/29 by kane@myriad
      
      	   * this marks 0.03
      
      Change 12126 on 2005/07/29 by kane@myriad
      
      	   * add C::D::Build to the dependencies, as it's released seperately from now
      	     on
      
      Change 11848 on 2005/01/14 by kane@myriad
      
      	   * add the mandatory 1; ;)
      
      Change 11847 on 2005/01/14 by kane@myriad
      
      	   * update the versions of some dependencies
      
      Change 11739 on 2004/12/26 by kane@myriad
      
      	* add bundle::cpanplus::dependencies
      
     
    
    
    git-svn-id: http://oss.dwim.org/bundle-cpanplus-dependencies@4 4dccba1d-3c1b-0410-aec4-feb514a8dabc

---
+++ lib/Bundle/CPANPLUS/Dependencies.pm
@@ -0,0 +1,98 @@
+package Bundle::CPANPLUS::Dependencies;
+
+$VERSION = "0.05";
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::CPANPLUS::Dependencies -- All modules required & desired by CPANPLUS
+
+=head1 SYNOPSIS
+
+    perl -MCPANPLUS -e 'install Bundle::CPANPLUS::Dependencies'
+
+    perl -MCPAN -e 'install Bundle::CPANPLUS::Dependencies'
+
+=head1 DESCRIPTION
+
+Bundle to install all modules required & desired by CPANPLUS 
+
+=head1 CONTENTS
+
+Archive::Extract 0.11
+
+Archive::Tar 1.23
+
+Archive::Zip 
+
+Compress::Zlib
+
+Digest::MD5
+
+File::Fetch 0.08
+
+IO::Zlib 1.04
+
+IPC::Cmd 0.24
+
+IPC::Run 0.77
+
+Locale::Maketext::Simple 0.01
+
+Log::Message 0.01
+
+LWP
+
+LWP::UserAgent
+
+Module::Build 0.2805
+
+Module::CoreList 1.97
+
+Module::Load 0.10
+
+Module::Load::Conditional 0.10
+
+Module::Loaded 0.01
+
+Module::Pluggable 2.4
+
+Module::Signature 0.44
+
+Net::FTP
+
+Object::Accessor 0.03
+
+Package::Constants 0.01
+
+Params::Check 0.22
+
+Storable
+
+Term::ReadLine::Perl
+
+Term::UI 0.05
+
+Test::Harness 2.28
+
+Test::More
+
+URI
+
+YAML
+
+CPANPLUS::Dist::Build 0.04
+
+Bundle::CPANPLUS::Test::Reporter
+
+=head1 AUTHOR
+
+Jos Boumans <kane@cpan.org>
+
+=cut 
+
+
+__END__
